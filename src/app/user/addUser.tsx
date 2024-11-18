99c88572634d1e21ffc5063882d3d076094504cc
BerlianPurliN <novtengpn14@gmail.com>
2024-11-18 13:57:30 +0700

    lanjut4

--- src/app/user/addUser.tsx
+++ src/app/user/addUser.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useEffect, useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "react-query";
 import { addUser, getUserById, updateUser } from "../service";
